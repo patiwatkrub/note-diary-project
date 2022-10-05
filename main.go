afcff01ca7b74b1f524c6e69e0cb224f819fcfce
KPatiwat <Patiwatkrubst@hotmail.com>
2022-10-06 00:10:47 +0700

    Added Features

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello Project!")
+	var iGreeting interface{} = "hi there!"
+	fmt.Println("Hello World!")
+
+	// type to check string with interface
+	if strGreet, ok := iGreeting.(string); ok {
+		fmt.Println(strGreet)
+	} else {
+		fmt.Println("something wrong!, iGreeting isn't string type")
+	}
+
 }
